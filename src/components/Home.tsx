ea3cea5e695e2a2ca9e8e5226d42ae0a1aad9941
adridalo <adrianodalonzo@gmail.com>
2024-09-19 19:22:41 -0400

    Add back to top button

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,15 @@
 import '../App.css'
 import About from './About'
+import Nav from './Nav'
 
 function Home() {
 
   return (
     <div>
       <h1 id='name-header'>Adriano D'Alonzo</h1>
+      <Nav />
       <About />
+      <p id='back-to-top'><a className='internal-link' href='#name-header'>Back to Top</a></p>
     </div>
   )
 }
