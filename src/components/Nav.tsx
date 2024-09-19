f33ca396e180c1fb83ad4be69459964f8fe32e68
adridalo <adrianodalonzo@gmail.com>
2024-09-19 19:23:06 -0400

    Create nav component. Add styling for transitions/animations + nav bar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+    return ( 
+        <nav id='navbar'>
+            <a className="internal-link" href="#about-container">About Me</a>
+            <a className="internal-link" href="#project-container">Projects</a>
+            <a className="internal-link" href="#interests-container">Interests</a>
+            <a className="internal-link" href="#contact-container">Contact</a>
+        </nav>   
+    );
+}
+ 
+export default Nav
