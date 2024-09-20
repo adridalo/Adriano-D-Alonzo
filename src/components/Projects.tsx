5ff751497b32b3c0d7a01d27b6a69c4e76246d92
adridalo <adrianodalonzo@hotmail.com>
2024-09-20 16:58:55 -0400

    Add class to project titles

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -36,23 +36,23 @@ const Projects = () => {
         <h1>Projects</h1>
         <div id="projects-section">
             <div className="project">
-                <a href="https://morsecodeencoder.onrender.com/" target="_href"><h2>Morse Code Encoder</h2></a>
-                <img className="project-demo" src="./images/project-demos/morsecode.png" alt="morse-code" />
+                <a draggable={false} className="project-title" href="https://morsecodeencoder.onrender.com/" target="_href"><h2>Morse Code Encoder</h2></a>
+                <img draggable={false} className="project-demo" src="./images/project-demos/morsecode.png" alt="morse-code" />
                 <p className="project-description">React application where the user can enter in some text and have it encoded for them in morse code!</p>
             </div>
             <div className="project">
-                <a href="https://adridalo.github.io/CompileChecker/" target="_href"><h2>Compile Checker</h2></a>
-                <img className="project-demo" src="./images/project-demos/compilechecker.gif" alt="compile-checker" />
+                <a draggable={false} className="project-title" href="https://adridalo.github.io/CompileChecker/" target="_href"><h2>Compile Checker</h2></a>
+                <img draggable={false} className="project-demo" src="./images/project-demos/compilechecker.gif" alt="compile-checker" />
                 <p className="project-description">Application where user can see if their source code compiles</p>
             </div>
             <div className="project">
-                <h2>Blackjack</h2>
-                <img className="project-demo" src="./images/project-demos/boiler.jpg" alt="boilerplate" />
+                <a draggable={false} className="project-title" href="#" target="_href"><h2>Blackjack</h2></a>
+                <img draggable={false} className="project-demo" src="./images/project-demos/boiler.jpg" alt="boilerplate" />
                 <p className="project-description">Vite React application game of the ever so infamous game, Blackjack!</p>
             </div>
             <div className="project">
-                <a href="https://adridalo.github.io/ShutTheBox/" target="_href"><h2>Shut The Box</h2></a>
-                <img className="project-demo" src="./images/project-demos/shutthebox.png" alt="shutthebox" />
+                <a draggable={false} className="project-title" href="https://adridalo.github.io/ShutTheBox/" target="_href"><h2>Shut The Box</h2></a>
+                <img draggable={false} className="project-demo" src="./images/project-demos/shutthebox.png" alt="shutthebox" />
                 <p className="project-description">ShutTheBox game written in ReactJS</p>
             </div>
         </div>
