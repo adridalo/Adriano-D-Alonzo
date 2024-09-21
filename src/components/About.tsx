c16e7bce9055f474ce6b39b5ac4ca9fd369436b5
adridalo <adrianodalonzo@gmail.com>
2024-09-21 00:09:36 -0400

    Change project desc and title color

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -42,7 +42,7 @@ const About = () => {
                     id="splash"
                     draggable={false}
                 />
-                <p id="about-info">{calculateAge()} year old software developer whos passionate about honing my skills, creating great things, and all things IT! Over the past {calculateTimeInCollege()} years, I've gathered up lots of experience with numerous technologies in all stacks of developement, specializing primarily in web development. I am familiar with both frontend & backend sectors of development, considering myself a Jack of all Trades when it comes to developing any sort of application. I strive to make innovative and appealing things, while at the same time always keep myself learning new technologies in the ever-so-expanding world of IT!</p>
+                <p id="about-info">I am a {calculateAge()} year old software developer whos passionate about honing my skills, creating great things, and all things IT! Over the past {calculateTimeInCollege()} years, I've gathered up lots of experience with numerous technologies in all stacks of developement, specializing primarily in web development. I am familiar with both frontend & backend sectors of development, considering myself a Jack of all Trades when it comes to developing any sort of application. I strive to make innovative and appealing things, while at the same time always keep myself learning new technologies in the ever-so-expanding world of IT!</p>
             </div>
         </div>
     );
